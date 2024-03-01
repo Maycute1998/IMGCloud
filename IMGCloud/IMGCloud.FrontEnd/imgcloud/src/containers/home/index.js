926cd98078d61fba2cd5617dbc1428534e6e2956
mng98 <mng18698@gmail.com>
2024-03-02 01:34:01 +0700

    init Frontend side

---
+++ IMGCloud/IMGCloud.FrontEnd/imgcloud/src/containers/home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "../post";
+import "./home.scss";
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Post />
+      </div>
+    );
+  }
+}
+
+export default Home;
