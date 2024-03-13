4854968f65d5271c31b617de3af1ab36ae01dd78
khoi.phan <khoi.phan@evcargo.com>
2024-03-13 16:11:57 +0700

    imp: fix code and add google authen

---
+++ src/IMGCloud.Infrastructure/Context/GoogleAuthenticationContext.cs
@@ -0,0 +1,7 @@
+﻿namespace IMGCloud.Infrastructure.Context;
+
+public sealed class GoogleAuthenticationContext
+{
+    public string Provider { get; set; } = string.Empty;
+    public string GoogleTokenId { get; set; } = string.Empty;
+}
